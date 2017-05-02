b22a0d40cdea70a0fb3ab8e74c809783f048a46f
= <baraunaluciano@gmail.com>
2017-05-02 07:53:29 -0300

    Subindo cap 18

---
+++ cap-18/testes-exvideo.js
@@ -0,0 +1,11 @@
+// Todo teste precisa de assercao (uma comparacao).
+
+var sum = function(value1, value2){
+    return value1 + value2;
+}
+
+QUnit.test('Teste da função sum', function(assert){
+    assert.equal(sum(2,3), 5, "sum(2,3) = 5");
+    assert.equal(sum(2,2), 4, "sun(2,2) = 4");
+    assert.notEqual(sum(2,2), 5, "sun(2,2) != 5");
+});
