142b35aff3fe0ad194ae27ca30ca49d0d3201415
= <baraunaluciano@gmail.com>
2017-05-03 07:50:38 -0300

    Continuando ex do cap 18

--- cap-18/testes-exvideo.js
+++ cap-18/testes-exvideo.js
@@ -4,8 +4,16 @@ var sum = function(value1, value2){
     return value1 + value2;
 }
 
+var mult = function(value1, value2){
+    return value1 * value2;
+}
+
 QUnit.test('Teste da função sum', function(assert){
     assert.equal(sum(2,3), 5, "sum(2,3) = 5");
     assert.equal(sum(2,2), 4, "sun(2,2) = 4");
     assert.notEqual(sum(2,2), 5, "sun(2,2) != 5");
+});
+
+QUnit.test('Teste da função mult', function(assert){
+
 });
