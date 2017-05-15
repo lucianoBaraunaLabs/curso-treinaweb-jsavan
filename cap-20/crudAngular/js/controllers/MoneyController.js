fd671aa89b50b09c5c541ee3127de35c5251b64f
= <baraunaluciano@gmail.com>
2017-05-14 22:22:22 -0300

    Continuando com o exemplo de CRUD

--- cap-20/crudAngular/js/controllers/MoneyController.js
+++ cap-20/crudAngular/js/controllers/MoneyController.js
@@ -1,15 +1,18 @@
 SaiaDoVermelho.controller("MoneyController",["$scope","MoneyService", function($scope, MoneyService){
-	$scope.appName = "Saia Do Vermelho";
+	$scope.money = MoneyService.money;
+	$scope.new = function (params) {
+		
+	}
 	
-	MoneyService.newIncoming({
-		name:"Salario",
-		value:2000
-	});
+	// MoneyService.newIncoming({
+	// 	name:"Salario",
+	// 	value:2000
+	// });
 	
-	MoneyService.newExpense({
-		name:"mercado",
-		value:100
-	});
+	// MoneyService.newExpense({
+	// 	name:"mercado",
+	// 	value:100
+	// });
 
 	console.log(MoneyService);
 
